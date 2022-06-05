02449328e3e908c8f0be27ed9423020a09b1b4b1
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-05 08:52:42 +0300

    Feat : Adds email comfiguration view function

--- lite/views.py
+++ lite/views.py
@@ -20,3 +20,11 @@ def create_profile(request):
     else:
         form = CreateProfileForm()
     return render(request, 'create-profile.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def email(request):
+    current_user = request.user
+    email = current_user.email
+    name = current_user.username
+    send_signup_email(name, email)
+    return redirect(create_profile)
