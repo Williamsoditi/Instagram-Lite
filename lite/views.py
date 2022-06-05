1b9638b23c6301b1a84792dbb6e3e50dfd8fc4fa
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-06-05 08:49:55 +0300

    Feat : Adds create profile view function

--- lite/views.py
+++ lite/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Image, Profile, Comment, Follow
 from .forms import CreateProfileForm, UploadImageForm, FollowForm, UnfollowForm, EditBioForm
+from django.http import HttpResponseRedirect, Http404
+from .emails import send_signup_email
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CreateProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = CreateProfileForm()
+    return render(request, 'create-profile.html', {"form": form})
