89cf72306853dc0ac09ebd92d329f65c930d3f7f
Williams <williamsoditi99@gmail.com>
2023-02-10 20:24:41 +0300

    fix apostrophe in mode

--- instagram/settings.py
+++ instagram/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-MODE = os.environ['MODE']
+MODE = os.environ["MODE"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
